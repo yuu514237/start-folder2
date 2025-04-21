f2a9a0ddb49fb2fbaed1960daf9ba1629f0c61d7
yuu514237 <mahu_3h@outlook.jp>
2025-04-21 20:19:19 +0900

    Create work1_6課題.js

---
+++ work1_6課題.js
@@ -0,0 +1,29 @@
+function makeCoffee() {
+  console.log("☕ スタッフ：先にコーヒーを準備します...");
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("✅ コーヒーができました！");
+      resolve();
+    }, 2000); // 2秒
+  });
+}
+
+function makeSandwich() {
+  console.log("🥪 スタッフ：次にサンドイッチを準備します...");
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("✅ サンドイッチができました！");
+      resolve();
+    }, 3000); // 3秒
+  });
+}
+
+async function serveCustomer() {
+  await Promise.all([makeCoffee(), makeSandwich()]);
+  console.log("🍽️ スタッフ：すべてできたので配膳します！");
+}
+
+console.log("🥪 お客さんがコーヒーとサンドイッチを注文しました");
+serveCustomer();
